8e6a97777074378a261c32bd36030a58e70280e3
Alexander Linder <alexander.linder@student.kit.edu>
2023-06-01 19:02:15 +0200

    Cleanup coordinator

--- coordinator/src/main/java/edu/kit/tm/ps/latte_mixxiato/coordinator/Main.java
+++ coordinator/src/main/java/edu/kit/tm/ps/latte_mixxiato/coordinator/Main.java
@@ -1,23 +1,18 @@
 package edu.kit.tm.ps.latte_mixxiato.coordinator;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.robertsoultanaev.javasphinx.SerializationUtils;
-import edu.kit.tm.ps.latte_mixxiato.lib.routing.InMemoryMixNodeRepository;
 import edu.kit.tm.ps.latte_mixxiato.lib.routing.MixNode;
 import org.eclipse.jetty.server.Response;
 import spark.Spark;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
         final var coordinator = new Coordinator();
-        Logger.getGlobal().info("Loaded mix data from disk.");
         Spark.get("/api/mixes/all", (req, res) -> {
             final var result = new JsonArray();
             coordinator.all().stream()
