10d9bb1d36e7dcc93281f80b4a8382dd3e01670a
Alexander Linder <alexander.linder@student.kit.edu>
2023-06-05 16:26:41 +0200

    Remove dead code

--- lib/src/main/java/edu/kit/tm/ps/latte_mixxiato/lib/routing/mix/DeadDrop.java
+++ lib/src/main/java/edu/kit/tm/ps/latte_mixxiato/lib/routing/mix/DeadDrop.java
@@ -2,13 +2,7 @@ package edu.kit.tm.ps.latte_mixxiato.lib.routing.mix;
 
 import com.google.gson.JsonObject;
 import com.robertsoultanaev.javasphinx.SerializationUtils;
-import com.robertsoultanaev.javasphinx.SphinxClient;
-import com.robertsoultanaev.javasphinx.SphinxException;
-import com.robertsoultanaev.javasphinx.packet.SphinxPacket;
 import org.bouncycastle.math.ec.ECPoint;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public record DeadDrop(String host, int relayPort, ECPoint publicKey) {
 
@@ -17,19 +11,6 @@ public record DeadDrop(String host, int relayPort, ECPoint publicKey) {
         final var port = json.get("gatewayPort").getAsInt();
         final var pubKey = SerializationUtils.decodeECPoint(SerializationUtils.base64decode(json.get("pubKey").getAsString()));
         return new DeadDrop(host, port, pubKey);
-    }
-
-    /**
-     * Sends a {@link SphinxPacket} via the given {@link SphinxClient}
-     * @param client client to use for sending
-     * @param packet packet to send
-     * @throws IOException thrown if an error occurs opening the connection to the host
-     */
-    public void send(SphinxClient client, SphinxPacket packet) throws IOException, SphinxException {
-        try (final var socket = new Socket(host, relayPort)) {
-            final var os = socket.getOutputStream();
-            os.write(client.packMessage(packet));
-        }
     }
 
     public JsonObject toJson() {
--- lib/src/main/java/edu/kit/tm/ps/latte_mixxiato/lib/routing/mix/Relay.java
+++ lib/src/main/java/edu/kit/tm/ps/latte_mixxiato/lib/routing/mix/Relay.java
@@ -2,13 +2,7 @@ package edu.kit.tm.ps.latte_mixxiato.lib.routing.mix;
 
 import com.google.gson.JsonObject;
 import com.robertsoultanaev.javasphinx.SerializationUtils;
-import com.robertsoultanaev.javasphinx.SphinxClient;
-import com.robertsoultanaev.javasphinx.SphinxException;
-import com.robertsoultanaev.javasphinx.packet.SphinxPacket;
 import org.bouncycastle.math.ec.ECPoint;
-
-import java.io.IOException;
-import java.net.Socket;
 
 public record Relay(String host, int gatewayPort, int deadDropPort, ECPoint publicKey) {
 
@@ -18,19 +12,6 @@ public record Relay(String host, int gatewayPort, int deadDropPort, ECPoint publ
         final var deadDropPort = json.get("deadDropPort").getAsInt();
         final var pubKey = SerializationUtils.decodeECPoint(SerializationUtils.base64decode(json.get("pubKey").getAsString()));
         return new Relay(host, gatewayPort, deadDropPort, pubKey);
-    }
-
-    /**
-     * Sends a {@link SphinxPacket} via the given {@link SphinxClient}
-     * @param client client to use for sending
-     * @param packet packet to send
-     * @throws IOException thrown if an error occurs opening the connection to the host
-     */
-    public void send(SphinxClient client, SphinxPacket packet) throws IOException, SphinxException {
-        try (final var socket = new Socket(host, gatewayPort)) {
-            final var os = socket.getOutputStream();
-            os.write(client.packMessage(packet));
-        }
     }
 
     public JsonObject toJson() {
